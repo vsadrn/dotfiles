569d876a3b05df3f5451b2e258aaeea46a8b54d0
Adrian Vesa <adrianvesa.ro@gmail.com>
2024-03-25 15:32:42 +0200

    Update

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,22 +1,30 @@
 return {
-	"EdenEast/nightfox.nvim",
-	priority = 1000,
-	config = function()
-		require("nightfox").setup({
-			options = {
-				transparent = false,
-				styles = {
-					comments = "italic",
-					keywords = "bold",
-					types = "italic,bold",
-				},
-			},
-			palettes = {
-				carbonfox = {
-					red = "#c94f6d",
-				},
-			},
-		})
-		vim.cmd.colorscheme("nightfox")
-	end,
+	-- {
+	-- 	"EdenEast/nightfox.nvim",
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		require("nightfox").setup({
+	-- 			options = {
+	-- 				transparent = false,
+	-- 				styles = {
+	-- 					comments = "italic",
+	-- 					keywords = "bold",
+	-- 					types = "italic,bold",
+	-- 				},
+	-- 			},
+	-- 			palettes = {
+	-- 				carbonfox = {
+	-- 					red = "#c94f6d",
+	-- 				},
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- },
+	{
+		"Mofiqul/dracula.nvim",
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("dracula")
+		end,
+	},
 }
